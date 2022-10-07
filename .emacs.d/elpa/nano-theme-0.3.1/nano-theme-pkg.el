a0597d15b77d126d53e2825b9480624d8936f0d3
Axel Karlsson <axel.v.m.karlsson@gmail.com>
2022-10-07 08:59:42 +0200

    Delete .emacs.d directory

--- .emacs.d/elpa/nano-theme-0.3.1/nano-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;; Generated package description from nano-theme.el  -*- no-byte-compile: t -*-
-(define-package "nano-theme" "0.3.1" "N Λ N O theme" '((emacs "27.1")) :commit "c4f296d349cf5ef2efd88d68535a4dbf577b9a87" :maintainer '("Nicolas P. Rougier" . "Nicolas.Rougier@inria.fr") :keywords '("theme" "dark" "light") :url "https://github.com/rougier/nano-theme")
